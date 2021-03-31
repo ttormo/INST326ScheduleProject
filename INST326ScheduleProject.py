d7c1cab5350b8a6c0fb9153c20e617c0b9936117
Alyson Flora <alysongracee@gmail.com>
2021-03-31 09:33:44 -0400

    new version of copy

---
+++ INST326ScheduleProject.py
@@ -0,0 +1,11 @@
+"""A simple scheduling program to assist INST majors"""
+
+
+class User:
+    """ A person using the scheduler.
+    
+    Attributes:
+    Name (str): The persons name
+    Classes (set of Person) : current classes of the user
+    """
+    
