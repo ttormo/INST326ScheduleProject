04be11b200850ae15089604ad425f414e0d48053
Bry'elle Monk <_bryelleee_@Bryelles-MacBook-Pro.local>
2021-03-31 09:52:47 -0400

    Bry'elle's update

--- INST326ScheduleProject.py
+++ INST326ScheduleProject.py
@@ -8,7 +8,12 @@ class User:
     Name (str): The persons name
     Classes (set of Person) : current classes of the user
     """
-
+class Schedule :
+    """ A student's schedule.
+        
+        Attributes:
+        Schedule (a file): a file of a schedule
+    """
     def __init__(self, name):
         self.name = name
 
